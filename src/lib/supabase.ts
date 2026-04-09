7cc6c5072f61c61c1259a86d7c4a2330064b202b
Zanteria <zwells@bxsom.com>
2026-04-08 20:17:52 -0500

    update blank screen attempt 1

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY ?? '';
+const supabaseUrl = 'https://hndvaeuamzyianladfia.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZHZhZXVhbXp5aWFubGFkZmlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2ODk1ODksImV4cCI6MjA5MTI2NTU4OX0.INRNQ4A42cHhkFcXEDMNiNYRhVYO6CiglyMK9gmpZ8w';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: { persistSession: false },
